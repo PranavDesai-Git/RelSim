9146ea7b484c8be5410c832f0fe96a86c19a56c3
Pranav Desai <contact.pranavdesai@gmail.com>
2026-08-10 00:24:55 +0530

    Initial Commit

---
+++ build/CMakeFiles/RelSym.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RelSym.dir/link.d"
+  "/home/host0/Projects/CPP/simulation/out/RelSym"
+  "/home/host0/Projects/CPP/simulation/out/RelSym.pdb"
+  "CMakeFiles/RelSym.dir/src/main.cpp.o"
+  "CMakeFiles/RelSym.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RelSym.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
